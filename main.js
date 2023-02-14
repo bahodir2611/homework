b416f0154693a0698ecefbbdbdb336b6a4934eac
Bahodir <92070480+bahodir2611@users.noreply.github.com>
2023-02-15 01:07:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,231 @@
+
+
+////////////////////////////// 1-misol/////////////////////
+
+//  var text = prompt("iltimos bironta son kiriting" + "");
+//  if (text > 0) {
+//     console.log("musbat");
+//  }else{
+//      console.log("manfiy");
+//  }
+
+
+
+//////////////////2 va 3 -misol//////////////////////////
+
+
+// var num = prompt("iltimos raqam1 ni kiriting" + "");
+// if (num % 2==0) {
+//     console.log('kiritilgan raqam juft ');
+// }else{
+//     console.log('kiritilgan raqam toq');
+// }
+
+////////////////////////////////////////////
+
+
+
+////////////////////4-misol////////////////////
+
+// let a = -4;
+// let b = 6;
+
+// if (a > 0 && b >0) {
+//     console.log("butun son");
+// }else{
+//     console.log("kiritilgan son 0 dan kichik");
+// }
+
+
+
+
+////////////// 5-misol ////////////////////////
+
+// const a = prompt("ilitmos son kiriting" + " ");
+// const b = prompt("ilitmos 2-soni kiriting" + " ");
+// const c = prompt("ilitmos 3-soni kiriting" + " ");
+
+// console.log(a , b ,c);
+
+
+
+// if(a < b && b < c && a<c){
+//     console.log("xatolik");
+// }else{
+//     console.log("tog\'ri");
+//     console.log(a + " "+ "katta" + " " + b + " " + "dan" + " " + c + " " +"kichik" + " " +a);
+// }
+
+
+
+
+/////////////////////6-misol///////////////////////
+
+// var a = 10;
+// var b = 13;
+// var c = 20;
+// if (b>a && b<c) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+/////////////////////7-misol//////////////////////
+
+// const a = prompt("ilitmos son kiriting" + " ");
+// console.log(a +" " + "% 2");
+
+// if (a % 2 == 1) {
+//     console.log("toq son");
+// }else{
+//     console.log("bu juft son");
+// }
+
+//////////////////8-misol////////////////////////
+
+// var a = -1;
+// var b = 2;
+// var c = 3;
+// var d = 4;
+
+// if (a > 0 && b >0 && c>0 && d>0) {
+//     console.log("sonlarning barchasi musbat qiymatga teng");
+// }else{
+//     console.log("sonlardan qaysi biridir manfiy qiymatga teng");
+// }
+
+
+//////////////////9-misol////////////////////////
+
+// var a = 10;
+// var b = 20;
+// var c = -10;
+//  if (a>=0 && b>=0 || c>=0) {
+//     console.log(true);
+//  }else{
+//     console.log(false);
+//  }
+
+
+
+
+//////////////////10-misol////////////////////////
+
+
+
+
+//////////////////11-misol////////////////////////
+
+// var simKarta = prompt("iltimos quyidagi formatda kodni kiriting 90,91,93,94,95,97,99,88,33" + "");
+
+// if (simKarta == 90 || simKarta == 91 ) {
+//     console.log("beeline");
+//     if (simKarta == 93 || simKarta == 94 ) {
+//         console.log("Ucell");
+//     }
+// }else{
+//     console.log("iltimos togri formatni korsating");
+// }
+
+
+//////////////////12-misol////////////////////////
+
+// var a = prompt("qanaqadir son" + "");
+// if (a > 0, a++) {
+//     console.log(a);
+// }else{
+//     a--;
+//     console.log(a);
+// }
+
+//////////////////13-misol////////////////////////
+
+// var a = prompt("son kiriting" + "");
+// if (a >= 0) {   
+//     console.log(Number(a)+3);
+// }else{
+//     //// bu yerda ikkita - minus birlashsa pilus qiymat qaytaradi shuning uchun + operator ishlatildi
+//     console.log(Number(a)+2);
+// }
+
+//////////////////14-misol////////////////////////
+
+// var a = prompt("a son kiriting " + "");
+// var b = prompt("b son kiriting " + "");
+
+// console.log("siz a qiymatga = "+ a + " || b qiymatga = " + b + " >> kiritdingiz");
+// if (a>b) {
+//     console.log(a + " bu son katta");
+// }else if (b>a) {
+//      console.log(b +" bunisi katta");
+// }
+
+//////////////////15-misol////////////////////////
+
+// var a = prompt("a son kiriting " + "");
+// var b = prompt("b son kiriting " + "");
+// var c = prompt("c son kiriting " + "");
+
+// console.log("siz a qiymatga = "+ a + " || b qiymatga = " + b + " c qiymatga = " + c + " >> kiritdingiz");
+// if (a>b && a>c) {
+//     console.log(a + " bu son katta");
+// }else if (b>a && b>c) {
+//      console.log(b +" bunisi katta");
+// }else if (c>a && c >b) {
+//      console.log(c +" bunisi katta");
+// }
+
+
+
+//////////////////16-misol////////////////////////
+
+
+// var a = prompt("a son kiriting " + "");
+// var b = prompt("b son kiriting " + "");
+// var c = prompt("c son kiriting " + "");
+
+// console.log("siz a qiymatga = "+ a + " || b qiymatga = " + b + " c qiymatga = " + c + " >> kiritdingiz");
+// if (a<b && a<c) {
+//     console.log(a + " bunisi kichik");
+// }else if (b<a && b<c) {
+//      console.log(b +" bunisi kichik");
+// }else if (c<a && c<b) {
+//      console.log(c +" bunisi kichik");
+// }
+
+
+//////////////////17-misol////////////////////////
+
+// var a = prompt('jinsingizni tasdiqlang misol "erkak" "ayol"' + "");
+// if(a="erkak" &&  confirm("boydoqmisiz")==true){ 
+//     console.log('uylaning');
+// }else{
+//     console.log("ikkinchisini bosvaring ministirlaga yarashoradi");
+// } 
+// if(a = "ayol" && confirm("turmush qurganmisiz")==true){
+//     console.log("tabriklayman !!!");
+// }else{
+//     console.log("erga teging");
+// }
+    
+   //////////////////18-misol////////////////////////
+
+
+//    var baho = prompt("bahoni kiriting " + "");
+
+//    if (Number(baho) == 5) {
+//     console.log(5 + " lik" + "stipendiya");
+//    }else if (Number(baho) == 4) {
+//     console.log(4 +" lik" + "stipendiya");
+//    }else if (Number(baho) <=3) {
+//     console.log(3 +"va" + 2+" lik" + "stipendiya");
+//    }
+
+   //////////////////19-misol////////////////////////
+   
+   
+   
+   
+   
+   //////////////////20-misol////////////////////////
